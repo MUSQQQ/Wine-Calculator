9fe3da7d99b86c8828b6dd605f4ca6df2501a98b
Jan Walczak <super.walczak@gmail.com>
2021-04-10 15:33:15 +0200

    Commit basic calculator model

---
+++ WineCalculator/Models/WineCalc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WineCalculator.Models
+{
+    public class WineCalc
+    {
+        [Required]
+        public string fruit { get; set; }
+        [Required]
+        public string volume { get; set; }
+        [Required]
+        public string power { get; set; }
+        public bool pasterized {get; set;}
+    }
+}
